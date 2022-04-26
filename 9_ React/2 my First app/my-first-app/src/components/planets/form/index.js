71054111c8fd03e31c9fa7aa16a5bd5e5df1fe4f
Diogo Araújo <dmaraujo.2011@gmail.com>
2022-04-26 18:40:34 -0300

    controlando multiplos inputs

--- 9_ React/2 my First app/my-first-app/src/components/planets/form/index.js
+++ 9_ React/2 my First app/my-first-app/src/components/planets/form/index.js
@@ -1,11 +1,22 @@
 import React, {Fragment, useState} from "react";
 
+const initialState = {
+  name: '',
+  description: '',
+  link: '',
+  img_url: ''
+}
+
 const Form = (props) =>{
-  const [name, setName] = useState('');
-  const handleChange = (event) => setName(event.target.value);
+  const [fields, setFields] = useState(initialState);
+  const handleFieldsChange = (e) => setFields({
+    ...fields,
+    [e.currentTarget.name]: e.currentTarget.value
+  });
   const handleSubmit = (event) => {
-    props.addPlanet({name: name});
+    props.addPlanet(fields);
     event.preventDefault()
+    setFields(initialState)
   }
 
   return(
@@ -13,7 +24,19 @@ const Form = (props) =>{
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">name: </label>
-          <input id='name'type='text' value={name} onChange={handleChange}/>
+          <input id='name' name='name' type='text' value={fields.name} onChange={handleFieldsChange}/>
+        </div>
+        <div>
+          <label htmlFor="paragrafo">description: </label>
+          <input id='paragrafo' name='paragrafo' type='text' value={fields.paragrafo} onChange={handleFieldsChange}/>
+        </div>
+        <div>
+          <label htmlFor="link">Link: </label>
+          <input id='link' name='link' type='text' value={fields.link} onChange={handleFieldsChange}/>
+        </div>
+        <div>
+          <label htmlFor="img_url">img_url </label>
+          <input id='img_url' name='img_url' type='text' value={fields.img_url} onChange={handleFieldsChange}/>
         </div>
         <br/>
         <input type='submit'/>
