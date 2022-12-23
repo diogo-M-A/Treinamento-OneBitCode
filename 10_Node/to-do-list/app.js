71bc876f5d81bc0cc99ea8ce8ca0fde65af4bc73
Diogo Araújo <dmaraujo.2011@gmail.com>
2022-12-23 02:26:07 -0300

    m1

--- 10_Node/to-do-list/app.js
+++ 10_Node/to-do-list/app.js
@@ -1,23 +1,9 @@
 const express = require('express')
-
+const checkListRouter = require('./src/routes/checklist')
 const app = express()
 app.use(express.json())
+app.use('/checklist', checkListRouter)
 
-const log = (req, res, next) => {
-  console.log(req.body)
-  console.log(`Data: ${Date.now()}`)
-  next()
-}
-app.use(log)
-
-app.get('/', (req, res)=>{
-  res.send('<h1>Minha lista de tarefas :)</h1>')
-})
-
-app.get('/json', (req, res)=>{
-  console.log(req.body)
-  res.json({name: 'Diogo', Birt: '17/06/2002'})
-})
 
 app.listen(3000, ()=>{
   console.log('O servidor foi iniciado')
